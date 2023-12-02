88978d11b67e0568743ae9e5c20947c76c76a73c
cocoyuy <xy2141@nyu.edu>
2023-12-02 11:56:58 +0800

    debug

--- xr-test-scene/js/main.js
+++ xr-test-scene/js/main.js
@@ -205,7 +205,7 @@ function updateThree() {
       direction.applyQuaternion(controller.quaternion); // apply the rotation of the controller 
 
       // generate a cube using the position and direction      
-      let object = new RandomObj()
+      let object = new Cube()
         .setPosition(position.x, position.y, position.z)
         .setVelocity(direction.x, direction.y, direction.z)
         .setRotationVelocity(random(-0.05, 0.05), random(-0.05, 0.05), random(-0.05, 0.05))
@@ -267,6 +267,101 @@ function updateThree() {
 
 function updateCamera() {
   camera.updateProjectionMatrix();
+}
+
+function getBox() {
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshLambertMaterial({
+    color: Math.random() * 0xffffff
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  return mesh;
+}
+
+class Cube {
+  constructor() {
+    this.pos = createVector();
+    this.vel = createVector();
+    this.acc = createVector();
+
+    this.scl = createVector(1, 1, 1);
+    this.mass = this.scl.x * this.scl.y * this.scl.z;
+
+    this.rot = createVector();
+    this.rotVel = createVector();
+    this.rotAcc = createVector();
+
+    this.lifespan = 1.0;
+    this.lifeReduction = random(0.005, 0.010);
+    this.isDone = false;
+    //
+    this.mesh = getBox();
+    scene.add(this.mesh);
+  }
+  setPosition(x, y, z) {
+    this.pos = createVector(x, y, z);
+    return this;
+  }
+  setVelocity(x, y, z) {
+    this.vel = createVector(x, y, z);
+    return this;
+  }
+  setRotationAngle(x, y, z) {
+    this.rot = createVector(x, y, z);
+    return this;
+  }
+  setRotationVelocity(x, y, z) {
+    this.rotVel = createVector(x, y, z);
+    return this;
+  }
+  setScale(w, h = w, d = w) {
+    const minScale = 0.01;
+    if (w < minScale) w = minScale;
+    if (h < minScale) h = minScale;
+    if (d < minScale) d = minScale;
+    this.scl = createVector(w, h, d);
+    this.mass = this.scl.x * this.scl.y * this.scl.z;
+    return this;
+  }
+  move() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+  rotate() {
+    this.rotVel.add(this.rotAcc);
+    this.rot.add(this.rotVel);
+    this.rotAcc.mult(0);
+  }
+  applyForce(f) {
+    let force = f.copy();
+    force.div(this.mass);
+    this.acc.add(force);
+  }
+  reappear() {
+    if (this.pos.z > WORLD_SIZE / 2) {
+      this.pos.z = -WORLD_SIZE / 2;
+    }
+  }
+  disappear() {
+    if (this.pos.z > WORLD_SIZE / 2) {
+      this.isDone = true;
+    }
+  }
+  age() {
+    this.lifespan -= this.lifeReduction;
+    if (this.lifespan <= 0) {
+      this.lifespan = 0;
+      this.isDone = true;
+    }
+  }
+  update() {
+    this.mesh.position.set(this.pos.x, this.pos.y, this.pos.z);
+    this.mesh.rotation.set(this.rot.x, this.rot.y, this.rot.z);
+
+    let newScale = p5.Vector.mult(this.scl, this.lifespan);
+    this.mesh.scale.set(newScale.x, newScale.y, newScale.z);
+  }
 }
 
 function getSphere() {
